021fc702c236e5291c4954cf1a42200c4c49bfc8
caleb-alberto <calberto3418@gmail.com>
2026-05-20 11:46:27 -0700

    update main

--- ClassProject/main.asm
+++ ClassProject/main.asm
@@ -26,12 +26,48 @@ isThree Byte "You have chosen option 3",0dh,0ah,0
 isFour Byte "You have chosen option 4",0dh,0ah,0
 isFive Byte "You have chosen option 5",0dh,0ah,0
 
+balance DWORD 0
+MAX_ALLOWED DWORD 20
+amount DWORD 0
+correctGuesses DWORD 0
+missedGuesses DWORD 0
+name BYTE 16 DUP(0)
+
+namePrompt BYTE "Please enter your name: ",0
+balanceMsg BYTE "Your available balance is: $",0
+addPrompt BYTE "Please enter the amount you would like to add: ",0
+addErrMsg BYTE "Error: Maximum allowable credit is $20.00.",0dh,0ah,
+               "Please enter a different amount and try again.",0dh,0ah,0
+guessPrompt BYTE "Please enter your guess (1-10): ",0
+winMsg BYTE "Congratulations! You guessed correctly! You win $2!",0dh,0ah,0
+lossMsg1 BYTE "Sorry, the correct number was: ",0
+lossMsg2 BYTE ". Better luck next time!",0dh,0ah,0
+playAgainMsg BYTE "Would you like to play again? (y/n): ",0
+statHeader BYTE "*** MY STATISTICS ***",0dh,0ah,0dh,0ah,0
+statNameLbl BYTE "Player Name:      ",0
+statBalLbl BYTE "Available Credit: $",0
+statPlayedLbl BYTE "Games Played:     ",0
+statCorrectLbl BYTE "Correct Guesses:  ",0
+statMissedLbl BYTE "Missed Guesses:   ",0
+statWonLbl BYTE "Money You Won:    $",0
+statLostLbl BYTE "Money You Lost:   $",0
+pressEnterMsg BYTE 0dh,0ah,"Press Enter to return to the main menu...",0
+
 .code
 main PROC
 
     mov edx, OFFSET team
     call WriteString
 
+    mov edx, OFFSET namePrompt
+    call WriteString
+    mov edx, OFFSET name
+    mov ecx, 15
+    call ReadString
+
+    call Randomize
+
+    menuLoop:
     mov edx, OFFSET menu
     call WriteString
 
@@ -44,7 +80,7 @@ main PROC
     jz choice3
     cmp eax, 4
     jz choice4
-    cmp eax, 5 
+    cmp eax, 5
     jz choice5
 
     mov edx, OFFSET invMsg
@@ -52,33 +88,132 @@ main PROC
     jmp endSwitch
 
     choice1:
-        mov edx, OFFSET isOne
+        mov edx, OFFSET balanceMsg
         call WriteString
+        mov eax, balance
+        call WriteDec
+        call Crlf
         jmp endSwitch
 
     choice2:
-        mov edx, OFFSET isTwo
+        mov edx, OFFSET addPrompt
+        call WriteString
+        call ReadInt
+        mov amount, eax
+        cmp eax, MAX_ALLOWED
+        jg addCreditErr
+        add balance, eax
+        jmp endSwitch
+
+    addCreditErr:
+        mov edx, OFFSET addErrMsg
         call WriteString
         jmp endSwitch
 
     choice3:
-        mov edx, OFFSET isThree
+        sub balance, 1
+
+        mov eax, 10
+        call RandomRange
+        inc eax
+        mov ebx, eax
+
+        mov edx, OFFSET guessPrompt
         call WriteString
+        call ReadInt
+
+        cmp eax, ebx
+        jz correctGuess
+
+    wrongGuess:
+        inc missedGuesses
+        mov edx, OFFSET lossMsg1
+        call WriteString
+        mov eax, ebx
+        call WriteDec
+        mov edx, OFFSET lossMsg2
+        call WriteString
+        jmp playAgain
+
+    correctGuess:
+        add balance, 2
+        inc correctGuesses
+        mov edx, OFFSET winMsg
+        call WriteString
+
+    playAgain:
+        mov edx, OFFSET playAgainMsg
+        call WriteString
+        call ReadChar
+        mov bl, al
+        call ReadChar      ; drain trailing newline
+        call Crlf
+        cmp bl, 'y'
+        jz choice3
+        cmp bl, 'Y'
+        jz choice3
         jmp endSwitch
 
     choice4:
-        mov edx, OFFSET isFour
+        mov edx, OFFSET statHeader
         call WriteString
+
+        mov edx, OFFSET statNameLbl
+        call WriteString
+        mov edx, OFFSET name
+        call WriteString
+        call Crlf
+
+        mov edx, OFFSET statBalLbl
+        call WriteString
+        mov eax, balance
+        call WriteDec
+        call Crlf
+
+        mov edx, OFFSET statPlayedLbl
+        call WriteString
+        mov eax, correctGuesses
+        add eax, missedGuesses
+        call WriteDec
+        call Crlf
+
+        mov edx, OFFSET statCorrectLbl
+        call WriteString
+        mov eax, correctGuesses
+        call WriteDec
+        call Crlf
+
+        mov edx, OFFSET statMissedLbl
+        call WriteString
+        mov eax, missedGuesses
+        call WriteDec
+        call Crlf
+
+        mov edx, OFFSET statWonLbl
+        call WriteString
+        mov eax, correctGuesses
+        add eax, eax
+        call WriteDec
+        call Crlf
+
+        mov edx, OFFSET statLostLbl
+        call WriteString
+        mov eax, correctGuesses
+        add eax, missedGuesses
+        call WriteDec
+        call Crlf
+
+        mov edx, OFFSET pressEnterMsg
+        call WriteString
+        call ReadChar
+        call Crlf
         jmp endSwitch
 
     choice5:
-        mov edx, OFFSET isFive
-        call WriteString
-        jmp endSwitch
-
+        exit
 
     endSwitch:
+    jmp menuLoop
 
-    exit
 main ENDP
 END main
